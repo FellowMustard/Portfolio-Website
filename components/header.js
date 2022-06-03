45190605666bdd6799a84fe6a07228acf47a956f
FellowMustard <rachmawanng33@gmail.com>
2022-06-03 15:22:59 +0700

    Meta tags revision

--- components/header.js
+++ components/header.js
@@ -11,6 +11,10 @@ function Header({ children }) {
           name="description"
           content="Hello!, if you're reading this, this is my personal portfolio created using Next Js."
         ></meta>
+        <meta
+          property="og:image"
+          content="https://i.ibb.co/dLRsL3Z/banner.png?resize=100"
+        ></meta>
       </Head>
       {children}
     </>
